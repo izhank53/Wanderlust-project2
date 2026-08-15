f3c9de05080dad04f4c3a54dde1adfa50a89c6b6
root <izhank53@gmail.com>
2026-08-15 05:49:23 +0000

    added AGCD

--- terraform/environments/dev/providers.tf
+++ terraform/environments/dev/providers.tf
@@ -10,22 +10,15 @@ provider "aws" {
   }
 }
 
-data "aws_eks_cluster" "main" {
-  name = "wanderlust-dev-cluster"
-}
-
-data "aws_eks_cluster_auth" "main" {
-  name = "wanderlust-dev-cluster"
-}
-
 provider "helm" {
   kubernetes = {
-    host = data.aws_eks_cluster.main.endpoint
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 
-    cluster_ca_certificate = base64decode(
-      data.aws_eks_cluster.main.certificate_authority[0].data
-    )
-
-    token = data.aws_eks_cluster_auth.main.token
+    exec = {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", var.aws_region]
+    }
   }
 }
--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -113,11 +113,16 @@ resource "helm_release" "aws_load_balancer_controller" {
 
 resource "helm_release" "metrics_server" {
   name       = "metrics-server"
-  namespace  = "kube-system"
   repository = "https://kubernetes-sigs.github.io/metrics-server/"
   chart      = "metrics-server"
+  namespace  = "kube-system"
+  version    = "3.12.1"
 
-  depends_on = [
-    aws_eks_node_group.main
+  # Using a YAML heredoc completely bypasses Terraform's block-parsing quirks
+  values = [
+    <<-EOT
+    args:
+      - --kubelet-insecure-tls
+    EOT
   ]
 }
